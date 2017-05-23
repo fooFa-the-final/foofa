e8ca8700e94691377df7d2f65487bd47f23643d8
Hyojin Kim <hg9829@gmail.com>
2017-05-23 17:31:06 +0900

    upload foodtruck, favorite sql

---
+++ FoodtruckFinderProject/WebContent/db/FoodtruckFavorite.sql
@@ -0,0 +1,16 @@
+CREATE TABLE foodtruck_tb(
+foodtruck_id VARCHAR2(100),
+seller_id VARCHAR2(100),
+truckName VARCHAR2(100),
+truck_image VARCHAR2(100),
+hour VARCHAR2(100),
+area VARCHAR2(100),
+notice VARCHAR2(100),
+location VARCHAR2(100),
+category VARCHAR2(100),
+payment boolean,
+parking boolean,
+drinking boolean,
+catering boolean,
+operation_state boolean
+);
