f12cb65b6e792edbbc0b67c91669a7ea70b4588d
hg9829 <hg9829@gmail.com>
2017-05-24 11:30:39 +0900

    Update Menu.java

--- FoodtruckFinderProject/src/fofa/domain/Menu.java
+++ FoodtruckFinderProject/src/fofa/domain/Menu.java
@@ -1,5 +1,38 @@
 package fofa.domain;
 
 public class Menu {
+	
+	private String menuId;
+	private String menuName;
+	private int price;
+	private boolean menuState;
+	
+	public String getMenuId() {
+		return menuId;
+	}
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+	public String getMenuName() {
+		return menuName;
+	}
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public boolean isMenuState() {
+		return menuState;
+	}
+	public void setMenuState(boolean menuState) {
+		this.menuState = menuState;
+	}
+	
+	
+	
 
 }
