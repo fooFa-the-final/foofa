b39c7b10106c8e03c22cf96ee2ffd98838802e76
DESKTOP-IUCQO8M\kostacom jung <wjdgnl13@gmail.com>
2017-05-24 11:35:21 +0900

    광고, 판매자 service, controller

--- FoodtruckFinderProject/src/fofa/service/SellerService.java
+++ FoodtruckFinderProject/src/fofa/service/SellerService.java
@@ -1,5 +1,17 @@
 package fofa.service;
 
+import fofa.domain.Seller;
+
 public interface SellerService {
 
+	
+	boolean register(Seller seller);
+	boolean modify(Seller seller);
+	boolean remove(String sellerId);
+	Seller findById(String sellerId);
+	boolean checkPw(String sellerId, String password);
+	boolean login(Seller seller);
+	String checkId(String sellerId);
+	
+	
 }
