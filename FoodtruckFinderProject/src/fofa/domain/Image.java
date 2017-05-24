14fa11815dfab828b799c34117187986743dbd0a
lsg7626 <dltmdrjs2000@naver.com>
2017-05-24 11:19:43 +0900

    Update Image.java

--- FoodtruckFinderProject/src/fofa/domain/Image.java
+++ FoodtruckFinderProject/src/fofa/domain/Image.java
@@ -1,5 +1,39 @@
 package fofa.domain;
 
 public class Image {
-
+	private String imageId;
+	private String category;
+	private String categoryId;
+	private String filename;
+	public String getImageId() {
+		return imageId;
+	}
+	public void setImageId(String imageId) {
+		this.imageId = imageId;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	@Override
+	public String toString() {
+		return "Image [imageId=" + imageId + ", category=" + category + ", categoryId=" + categoryId + ", filename="
+				+ filename + "]";
+	}
+	
+	
 }
