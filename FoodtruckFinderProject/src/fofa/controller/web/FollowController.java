8772e8d70c72998095566d8b1e9bedcb6fe388e0
kosta <kosta@DESKTOP-GFR28TA>
2017-05-24 12:27:49 +0900

    맴버 팔로우 매퍼친구들

---
+++ FoodtruckFinderProject/src/fofa/controller/web/FollowController.java
@@ -0,0 +1,20 @@
+package fofa.controller.web;
+
+import javax.servlet.http.HttpSession;
+
+public class FollowController {
+
+	public boolean create(String followingId, HttpSession session){
+		return false;
+	}
+	public boolean remove(String followingId, HttpSession session){
+		return false;
+	}
+	public String search(HttpSession session){
+		return null;
+	}
+	public int searchCount(String memberId){
+		return 0;
+	}
+	
+}
