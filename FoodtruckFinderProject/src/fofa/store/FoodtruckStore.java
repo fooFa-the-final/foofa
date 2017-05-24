b4a160f0a7bd81141d3639c63b9e9284aac1925d
hg9829 <hg9829@gmail.com>
2017-05-24 11:31:29 +0900

    Update FoodtruckStore.java

--- FoodtruckFinderProject/src/fofa/store/FoodtruckStore.java
+++ FoodtruckFinderProject/src/fofa/store/FoodtruckStore.java
@@ -1,5 +1,23 @@
 package fofa.store;
 
+import java.util.List;
+
+import fofa.domain.Foodtruck;
+
 public interface FoodtruckStore {
+	
+	int insert(Foodtruck foodtruck);
+	
+	int update(Foodtruck foodtruck);
+	
+	Foodtruck selectById(String foodtruckId);
+	
+	List<Foodtruck> selectByLoc(String location);
+	
+	List<Foodtruck> selectByKeyLoc(String keyword, String location);
+	
+	List<Foodtruck> selectByFilter(Foodtruck foodtruck);
+	
+	Foodtruck selectBySellerId(String sellerId);
 
 }
