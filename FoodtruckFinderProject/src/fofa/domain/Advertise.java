2b46ae3cc75772598917b623ed7c8f484b56e818
DESKTOP-IUCQO8M\kostacom jung <wjdgnl13@gmail.com>
2017-05-24 10:21:14 +0900

    중간백업 - service,store interface생성

--- FoodtruckFinderProject/src/fofa/domain/Advertise.java
+++ FoodtruckFinderProject/src/fofa/domain/Advertise.java
@@ -2,4 +2,33 @@ package fofa.domain;
 
 public class Advertise {
 
+	private String advId;
+	private String sellerId;
+	private String period;
+	private Boolean approve;
+	public String getAdvId() {
+		return advId;
+	}
+	public void setAdvId(String advId) {
+		this.advId = advId;
+	}
+	public String getSellerId() {
+		return sellerId;
+	}
+	public void setSellerId(String sellerId) {
+		this.sellerId = sellerId;
+	}
+	public String getPeriod() {
+		return period;
+	}
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+	public Boolean getApprove() {
+		return approve;
+	}
+	public void setApprove(Boolean approve) {
+		this.approve = approve;
+	}
+	
 }
