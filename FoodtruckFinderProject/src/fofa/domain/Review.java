6b3ed7303e9eacc87f5400b8ad54e364f10b514f
lsg7626 <dltmdrjs2000@naver.com>
2017-05-24 11:17:47 +0900

    Update Review.java

--- FoodtruckFinderProject/src/fofa/domain/Review.java
+++ FoodtruckFinderProject/src/fofa/domain/Review.java
@@ -1,6 +1,62 @@
-
 package fofa.domain;
 
-public class Review {
+import java.util.List;
 
+public class Review {
+	private String reviewId;
+	private String foodtruckId;
+	private String contents;
+	private int score;
+	private List<Image> images;
+	private Member writer;
+	private int recommand;
+	public String getReviewId() {
+		return reviewId;
+	}
+	public void setReviewId(String reviewId) {
+		this.reviewId = reviewId;
+	}
+	public String getFoodtruckId() {
+		return foodtruckId;
+	}
+	public void setFoodtruckId(String foodtruckId) {
+		this.foodtruckId = foodtruckId;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public List<Image> getImages() {
+		return images;
+	}
+	public void setImages(List<Image> images) {
+		this.images = images;
+	}
+	public Member getWriter() {
+		return writer;
+	}
+	public void setWriter(Member writer) {
+		this.writer = writer;
+	}
+	public int getRecommand() {
+		return recommand;
+	}
+	public void setRecommand(int recommand) {
+		this.recommand = recommand;
+	}
+	@Override
+	public String toString() {
+		return "Review [reviewId=" + reviewId + ", foodtruckId=" + foodtruckId + ", contents=" + contents + ", score="
+				+ score + ", images=" + images + ", writer=" + writer + ", recommand=" + recommand + "]";
+	}
+	
+	
 }
