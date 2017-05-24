f56ebb7c15a4fcb4aca2c6abab3a0806aa0abf53
hg9829 <hg9829@gmail.com>
2017-05-24 11:31:09 +0900

    Update FavoriteStore.java

--- FoodtruckFinderProject/src/fofa/store/FavoriteStore.java
+++ FoodtruckFinderProject/src/fofa/store/FavoriteStore.java
@@ -1,5 +1,17 @@
 package fofa.store;
 
+import java.util.List;
+
+import fofa.domain.Favorite;
+
 public interface FavoriteStore {
+	
+	int insert(Favorite favorite);
+	
+	int delete(Favorite favorite);
+	
+	List<Favorite> selectByMemberId(String memberId);
+	
+	int selectByTruckId(String foodtruckId);
 
 }
