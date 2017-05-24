1ff1c132b1fedb1620353e5f47b0959c69ca7b2b
juhee <juhee@218.38.137.28>
2017-05-24 16:40:05 +0900

    survey update

--- FoodtruckFinderProject/src/fofa/store/SurveyStore.java
+++ FoodtruckFinderProject/src/fofa/store/SurveyStore.java
@@ -8,6 +8,6 @@ public interface SurveyStore {
 
 	public int insert(Survey survey);
 	public List<Survey> selectByTruckId(String foodtruckId);
-	public List<Survey> selectAvgByAges(String foodtruckId);
-	public List<Survey> selectAvgByGender(String foodtruckId);
+	public List<Survey> selectAvgByAges(String foodtruckId, String itemId);
+	public List<Survey> selectAvgByGender(String foodtruckId, String itemId);
 }
