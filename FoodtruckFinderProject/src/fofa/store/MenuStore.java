e00a96b812245a72b2f9c5ec89053cdc3f67629d
hg9829 <hg9829@gmail.com>
2017-05-24 11:26:54 +0900

    Update MenuStore.java

--- FoodtruckFinderProject/src/fofa/store/MenuStore.java
+++ FoodtruckFinderProject/src/fofa/store/MenuStore.java
@@ -1,5 +1,17 @@
 package fofa.store;
 
+import java.util.List;
+
+import fofa.domain.Menu;
+
 public interface MenuStore {
+	
+	int insert(Menu menu);
+	
+	int update(Menu menu);
+	
+	int delete(String menuId);
+	
+	List<Menu> selectByTruckId(String foodtruckId);
 
 }
