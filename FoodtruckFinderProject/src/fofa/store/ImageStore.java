c5ebe2d859596563b3abdeb53fc39e76fda20e7e
lsg7626 <dltmdrjs2000@naver.com>
2017-05-24 11:22:25 +0900

    Update ImageStore.java

--- FoodtruckFinderProject/src/fofa/store/ImageStore.java
+++ FoodtruckFinderProject/src/fofa/store/ImageStore.java
@@ -1,5 +1,13 @@
 package fofa.store;
 
-public interface ImageStore {
+import java.util.List;
 
+import fofa.domain.Image;
+
+public interface ImageStore {
+	List<Image> selectReviewImage(String reviewId);
+	int insertImage(String reviewId, Image image);
+	int updateImage(Image image);
+	int deleteImage(String imageId);
 }
+
