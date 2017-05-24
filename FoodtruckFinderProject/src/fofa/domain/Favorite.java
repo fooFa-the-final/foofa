d5df3ccd8102a8dd6dd239a9dbc330f8b401ae3f
hg9829 <hg9829@gmail.com>
2017-05-24 11:20:46 +0900

    Update Favorite.java

--- FoodtruckFinderProject/src/fofa/domain/Favorite.java
+++ FoodtruckFinderProject/src/fofa/domain/Favorite.java
@@ -5,4 +5,20 @@ public class Favorite {
 	private String foodtruckId;
 	private String memberId;
 	
+	
+	public String getFoodtruckId() {
+		return foodtruckId;
+	}
+	public void setFoodtruckId(String foodtruckId) {
+		this.foodtruckId = foodtruckId;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	
+	
+	
 }
