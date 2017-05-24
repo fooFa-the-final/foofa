ee1b3fbe9fff0f29205a6196cc69492775fba23f
lsg7626 <dltmdrjs2000@naver.com>
2017-05-24 11:19:19 +0900

    Update Recommand.java

--- FoodtruckFinderProject/src/fofa/domain/Recommand.java
+++ FoodtruckFinderProject/src/fofa/domain/Recommand.java
@@ -1,5 +1,25 @@
 package fofa.domain;
 
 public class Recommand {
-
+	private String reviewId;
+	private String writerId;
+	public String getReviewId() {
+		return reviewId;
+	}
+	public void setReviewId(String reviewId) {
+		this.reviewId = reviewId;
+	}
+	public String getWriterId() {
+		return writerId;
+	}
+	public void setWriterId(String writerId) {
+		this.writerId = writerId;
+	}
+	@Override
+	public String toString() {
+		return "Recommand [reviewId=" + reviewId + ", writerId=" + writerId + "]";
+	}
+	
+	
 }
+
