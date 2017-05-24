6231a0ea8c06728c229c1052a7e1a92de83ce10c
juhee <juhee@218.38.137.28>
2017-05-24 14:37:20 +0900

    txt update

--- FoodtruckFinderProject/src/fofa/controller/web/SurveyController.java
+++ FoodtruckFinderProject/src/fofa/controller/web/SurveyController.java
@@ -1,16 +1,36 @@
 package fofa.controller.web;
 
 
+import java.util.List;
+
 import org.springframework.ui.Model;
 
 import fofa.domain.Survey;
+import fofa.domain.SurveyItem;
 
 public class SurveyController {
 
 	public String create(Survey survey, Model model){
 		
 	return null;
+	}
 	
+	public String searchSurveysStat(String foodtruckId, Model model){
+		return null;
+	}
 	
+	public String searchItemStat(String foodtruckId, String ItemId, Model model){
+		return null;
+	}
+	
+	public List<SurveyItem> createItem(String question){
+		return null;
+	}
+	
+	public List<SurveyItem> modifyItem(String question){
+		return null;
+	}	
+	public List<SurveyItem> removeItem(String question){
+		return null;
 	}
 }
