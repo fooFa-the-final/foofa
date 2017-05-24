da8ce0947bee196b77976f0d0f05fac63c0f6849
hg9829 <hg9829@gmail.com>
2017-05-24 11:32:11 +0900

    Update FavoriteService.java

--- FoodtruckFinderProject/src/fofa/service/FavoriteService.java
+++ FoodtruckFinderProject/src/fofa/service/FavoriteService.java
@@ -1,5 +1,17 @@
 package fofa.service;
 
+import java.util.List;
+
+import fofa.domain.Favorite;
+
 public interface FavoriteService {
+	
+	boolean register(Favorite favorite);
+	
+	boolean remove(Favorite favorite);
+	
+	List<Favorite> findMemberId(String memberId);
+	
+	int findByFoodtruckId(String foodtruckId);
 
 }
