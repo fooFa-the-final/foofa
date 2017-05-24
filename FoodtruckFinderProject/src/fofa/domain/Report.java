c98c64ceaf781c52bea9c6e136385fb673ee238e
lsg7626 <dltmdrjs2000@naver.com>
2017-05-24 11:18:19 +0900

    Update Report.java

--- FoodtruckFinderProject/src/fofa/domain/Report.java
+++ FoodtruckFinderProject/src/fofa/domain/Report.java
@@ -1,5 +1,32 @@
 package fofa.domain;
 
 public class Report {
-
+	private String reviewId;
+	private String reason;
+	private String memberId;
+	public String getReviewId() {
+		return reviewId;
+	}
+	public void setReviewId(String reviewId) {
+		this.reviewId = reviewId;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	@Override
+	public String toString() {
+		return "Report [reviewId=" + reviewId + ", reason=" + reason + ", memberId=" + memberId + "]";
+	}
+	
+	
 }
+
