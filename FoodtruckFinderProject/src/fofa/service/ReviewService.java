dd8d451fb506d6454e8af7b6f3cda815e68ed601
lsg7626 <dltmdrjs2000@naver.com>
2017-05-24 11:20:29 +0900

    Update ReviewService.java

--- FoodtruckFinderProject/src/fofa/service/ReviewService.java
+++ FoodtruckFinderProject/src/fofa/service/ReviewService.java
@@ -1,5 +1,23 @@
 package fofa.service;
 
-public interface ReviewService {
+import java.util.List;
 
+import fofa.domain.Recommand;
+import fofa.domain.Report;
+import fofa.domain.Review;
+
+public interface ReviewService {
+	boolean register(Review review);
+	boolean modify(Review review);
+	boolean remove(String reviewId);
+	Review findById(String reivewId);
+	List<Review> findByMemberId(String memberId);
+	List<Review> findByFromId(String memberId);
+	List<Review> findByTruckId(String foodtruckId);
+	List<Review> findByRecommand();
+	List<Review> findAllByReported();
+	boolean registerReport(Report report);
+	Report findReport(String reportId);
+	boolean registerRecommand(Recommand recommand);
+	boolean deleteRecommand(Recommand recommand);
 }
