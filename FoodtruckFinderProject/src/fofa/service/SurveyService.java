16b2d649509dc548d51ff448d9cf1c6ca9d1a273
juhee <juhee@218.38.137.28>
2017-05-24 12:14:52 +0900

    설문조사 완

--- FoodtruckFinderProject/src/fofa/service/SurveyService.java
+++ FoodtruckFinderProject/src/fofa/service/SurveyService.java
@@ -1,5 +1,16 @@
 package fofa.service;
 
+import java.util.List;
+
+import fofa.domain.Survey;
+import fofa.domain.SurveyReply;
+
 public interface SurveyService {
 
+	public boolean register(Survey survey);
+	public List<Survey> findByTruckId(String foodtruckId);
+	public List<Survey> findAvgByAges(String foodtruckId);
+	public List<Survey> findAvgByGender(String foodtruckId, String itemId);
+	public List<SurveyReply> findAvgScoreBySurveyItem(String foodtruckId);
+	
 }
--- FoodtruckFinderProject/src/fofa/store/SurveyStore.java
+++ FoodtruckFinderProject/src/fofa/store/SurveyStore.java
@@ -1,5 +1,13 @@
 package fofa.store;
 
+import java.util.List;
+
+import fofa.domain.Survey;
+
 public interface SurveyStore {
 
+	public int insert(Survey survey);
+	public List<Survey> selectByTruckId(String foodtruckId);
+	public List<Survey> selectAvgByAges(String foodtruckId);
+	public List<Survey> selectAvgByGender(String foodtruckId);
 }
