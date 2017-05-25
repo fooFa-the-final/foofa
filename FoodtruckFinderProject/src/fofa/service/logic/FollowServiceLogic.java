c320ace59f46284e7bcdf28811c0307775863252
kosta <kosta@DESKTOP-GFR28TA>
2017-05-25 14:45:17 +0900

    서비스 로직

--- FoodtruckFinderProject/src/fofa/service/logic/FollowServiceLogic.java
+++ FoodtruckFinderProject/src/fofa/service/logic/FollowServiceLogic.java
@@ -2,33 +2,39 @@ package fofa.service.logic;
 
 import java.util.List;
 
+import org.apache.catalina.Store;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import fofa.domain.Follow;
 import fofa.service.FollowService;
+import fofa.store.FollowStore;
 
+@Service
 public class FollowServiceLogic implements FollowService{
 
+	@Autowired
+	public FollowStore store;
+	
 	@Override
 	public boolean register(Follow follow) {
-		// TODO Auto-generated method stub
-		return false;
+	
+		return store.insert(follow) >0;
 	}
 
 	@Override
 	public boolean remove(Follow follow) {
-		// TODO Auto-generated method stub
-		return false;
+		return store.delete(follow)>0;
 	}
 
 	@Override
 	public List<Follow> findFollow(String toId) {
-		// TODO Auto-generated method stub
-		return null;
+		return store.selectByToId(toId);
 	}
 
 	@Override
 	public int findFollowsCount(String toId) {
-		// TODO Auto-generated method stub
-		return 0;
+		return findFollowsCount(toId);
 	}
 
 }
