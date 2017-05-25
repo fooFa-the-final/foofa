5fbb94bc9895144d7221a8a7751f7421d2d96826
Hyojin Kim <hg9829@gmail.com>
2017-05-25 17:34:10 +0900

    효진

--- FoodtruckFinderProject/src/fofa/controller/web/FavoriteController.java
+++ FoodtruckFinderProject/src/fofa/controller/web/FavoriteController.java
@@ -5,11 +5,14 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import fofa.service.FavoriteService;
 import fofa.service.FoodtruckService;
 import fofa.service.MemberService;
 
+@RequestMapping("/favorite")
 @Controller
 public class FavoriteController {
 	@Autowired
@@ -19,24 +22,22 @@ public class FavoriteController {
 	@Autowired
 	private MemberService memberService;
 	
+	@RequestMapping(value="/create.do", method=RequestMethod.GET)
 	public boolean create(HttpSession session, String foodtruckId){
 		
 		return false;
 	}
 
+	@RequestMapping(value="/remove.do", method=RequestMethod.GET)
 	public boolean remove(HttpSession session, String foodtruckId){
 		
 		return false;
 	}
 	
+	@RequestMapping(value="/list.do", method=RequestMethod.GET)
 	public String search(HttpSession session, Model model){
 		
-		return null;
-	}
-	
-	public int searchCount(String foodtruckId){
-		
-		return 0;
+		return "memberFavoriteList.jsp";
 	}
 	
 }
--- FoodtruckFinderProject/src/fofa/controller/web/FoodtruckController.java
+++ FoodtruckFinderProject/src/fofa/controller/web/FoodtruckController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import fofa.domain.Foodtruck;
 import fofa.domain.Menu;
@@ -14,6 +16,7 @@ import fofa.service.AdvertiseService;
 import fofa.service.FavoriteService;
 import fofa.service.FoodtruckService;
 
+@RequestMapping("/foodtruck")
 @Controller
 public class FoodtruckController {
 	@Autowired
@@ -23,61 +26,74 @@ public class FoodtruckController {
 	@Autowired
 	private AdvertiseService advertiseService;
 	
+	
+	@RequestMapping(value="/create.do", method=RequestMethod.GET)
 	public String create(Foodtruck foodtruck){
 		
-		return null;
+		return "login.jsp";
 	}
 	
+	@RequestMapping(value="/modifyForm.do", method=RequestMethod.GET)
 	public String modifyForm(String foodtruckId, Model model){
 		
-		return null;
+		return "modifyFoodtruck.jsp";
 	}
 	
+	@RequestMapping(value="/modify.do", method=RequestMethod.POST)
 	public String modify(Foodtruck foodtruck){
 		
-		return null;
+		return "foodtruckInfo.jsp";
 	}
 	
+	@RequestMapping(value="/modifyPicture.do", method=RequestMethod.GET)
 	public String modifyPicture(String foodtruckId, String file_name){
 		
 		return null;
 	}
 	
+	@RequestMapping(value="/modifyPicture.do", method=RequestMethod.GET)
 	public boolean modifyState(String foodtruckId, boolean operation_state){
 		
 		return false;
 	}
 	
+	@RequestMapping("/searchById.do")
 	public String searchById(String foodtruckId, Model model){
 		
-		return null;
+		return "foodtruckInfo.jsp";
 	}
 	
+	@RequestMapping("/searchByLoc.do")
 	public String searchByLoc(String location, Model model){
 		
-		return null;
+		return "listFoodtruck.jsp";
 	}
 	
+	@RequestMapping("/searchByKeyLoc.do")
 	public String searchByKeyLoc(String keyword, String location, Model model){
 		
-		return null;
+		return "listFoodtruck.jsp";
 	}
 	
+	@RequestMapping("/searchByFilter.do")
 	public String searchByFilter(HttpServletRequest request, Model model){
 		
-		return null;
+		return "listFoodtruck.jsp";
 	}
 	
+	@RequestMapping("/createMenu.do")
 	public List<Menu> createMenu(Menu menu){
 		
 		return null;
 	}
 	
+	@RequestMapping("/modifyMenu.do")
 	public List<Menu> modifyMenu(Menu menu){
 		
 		return null;
 	}
 	
+	@RequestMapping("/removeMenu.do")
 	public List<Menu> removeMenu(String menuId, String foodtruckId){
 		
 		return null;
