6ecb8db2cc4f8f04dec9eaafbc419430ca987154
BillGates <BillGates@BillGates>
2017-05-29 12:11:59 +0900

    detailFoodtruck.jsp, controller 추가

--- FoodtruckFinderProject/src/fofa/controller/web/ReviewController.java
+++ FoodtruckFinderProject/src/fofa/controller/web/ReviewController.java
@@ -8,17 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fofa.domain.Foodtruck;
 import fofa.domain.Recommand;
 import fofa.domain.Report;
 import fofa.domain.Review;
 import fofa.domain.Survey;
+import fofa.service.FoodtruckService;
 import fofa.service.ReviewService;
 
 @Controller
 public class ReviewController {
 	
 	@Autowired
-	private ReviewService service;
+	private ReviewService reviewService;
+	
+	@Autowired
+	private FoodtruckService truckService;
 	
 	@RequestMapping("/review/list/member.do")
 	public String searchByMemberId(String memberId, Model model){
@@ -27,7 +32,7 @@ public class ReviewController {
 
 	@RequestMapping("/review/list/follow.do")
 	public String searchByFollower(String memberId, Model model){
-		List<Review> list = service.findByFromId(memberId);
+		List<Review> list = reviewService.findByFromId(memberId);
 		System.out.println(list.size());
 		for(Review r : list){
 			System.out.println(r.getFoodtruck().getFoodtruckName());
@@ -38,6 +43,10 @@ public class ReviewController {
 	
 	@RequestMapping("/review/list/truck.do")
 	public String searchByFoodtruckId(String foodtruckId, Model model){
+		Foodtruck truck = truckService.findById(foodtruckId);
+		List<Review> reviewList = reviewService.findByTruckId(foodtruckId);
+		model.addAttribute("truck", truck);
+		model.addAttribute("reviewList", reviewList);
 		return "../../view/foodtruck/detailFoodtruck.jsp";
 	}
 	@RequestMapping(value="/review/create.do", method=RequestMethod.GET)
