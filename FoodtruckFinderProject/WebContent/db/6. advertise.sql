896833c7e98f8e346001f05a991789e28e204c64
Hyojin Kim <hg9829@gmail.com>
2017-05-30 11:56:16 +0900

    효진

--- FoodtruckFinderProject/WebContent/db/6. advertise.sql
+++ FoodtruckFinderProject/WebContent/db/6. advertise.sql
@@ -4,8 +4,8 @@ DROP SEQUENCE ADVERTISE_SEQ_ADV;
 create table adv_tb(
   adv_id varchar2(50) primary key,
   seller_id varchar2(50),
-  period varchar2(50),
-  start_date varchar2(50),
+  period NUMBER,
+  start_date DATE,
   confirm varchar2(1)
 );
 
@@ -18,13 +18,13 @@ ALTER TABLE adv_tb
 ADD FOREIGN KEY (seller_id) REFERENCES SELLER_TB(seller_id) on delete cascade;
 
 --광고등록
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'nacho', '1', '2017052013', '1');
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'pasta', '4', '2017052113', '0');
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'ginger', '5', '2017052913', '1');
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'pudding', '3', '2017052611', '0');
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'cholate', '6', '2017052513', '1');
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'gem', '3', '2017052213', '0');
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'jelly', '12', '2017052817', '1');
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'hotdog', '5', '2017052616', '0');
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'noodle', '6', '2017052513', '0');
-insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'candy', '9', '2017052413', '1');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'nacho', '1', '17/05/20', '1');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'pasta', '4', '17/05/21', '0');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'ginger', '5', '17/05/29', '1');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'pudding', '3', '17/05/26', '0');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'cholate', '6', '17/05/25', '1');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'gem', '3', '17/05/22', '0');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'jelly', '12', '17/05/28', '1');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'hotdog', '5', '17/05/26', '0');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'noodle', '6', '17/05/25', '0');
+insert into adv_tb VALUES ('A'||ADVERTISE_SEQ_ADV.NEXTVAL, 'candy', '9', '17/05/24', '1');
