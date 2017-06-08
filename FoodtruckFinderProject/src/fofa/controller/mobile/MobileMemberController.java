a67148c638a2145997680bc8b34d1ec29840124e
DESKTOP-IUCQO8M\kostacom jung <wjdgnl13@gmail.com>
2017-06-08 17:54:39 +0900

    모바일 컨트롤러

--- FoodtruckFinderProject/src/fofa/controller/mobile/MobileMemberController.java
+++ FoodtruckFinderProject/src/fofa/controller/mobile/MobileMemberController.java
@@ -1,5 +1,42 @@
 package fofa.controller.mobile;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fofa.domain.Member;
+import fofa.domain.Seller;
+import fofa.service.MemberService;
+import fofa.service.SellerService;
+
+@Controller
 public class MobileMemberController {
 
+	@Autowired
+	private MemberService memberService;
+	
+
+	
+	@RequestMapping(value="/mobile/mlogin.do")
+	public @ResponseBody String memberLogin(String id, String password) {
+
+		Member member = new Member();
+		member.setMemberId(id);
+		member.setPassword(password);
+
+		boolean result;
+		
+		result = memberService.checkPw(id, password);
+		
+		if(result == false) {
+			return "false";
+		} else {
+			return "true";
+		}
+		
+	}	
+
+
+	
 }
