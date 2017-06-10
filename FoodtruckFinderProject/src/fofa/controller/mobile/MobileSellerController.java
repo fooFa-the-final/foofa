0557dde094a9daf0b0fc0047f55b5d28079351d8
DESKTOP-IUCQO8M\kostacom jung <wjdgnl13@gmail.com>
2017-06-10 15:03:40 +0900

    truck xml

--- FoodtruckFinderProject/src/fofa/controller/mobile/MobileSellerController.java
+++ FoodtruckFinderProject/src/fofa/controller/mobile/MobileSellerController.java
@@ -48,6 +48,13 @@ public class MobileSellerController {
 			truck = truckService.findById(id);
 			return truck;
 	}
+	
+	@RequestMapping(value = "/mobile/detail.do", produces="application/xml")
+	public @ResponseBody Foodtruck detailTruck(String id){
+		Foodtruck foodtruck = truckService.findBySeller(id);
+		return foodtruck;
+	}
+	
 }			
 	
 	
