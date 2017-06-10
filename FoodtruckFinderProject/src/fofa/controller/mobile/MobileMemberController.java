3f80ccc4f7f7fbc9368b51c253ff278bd7f76e96
lsg7626 <dltmdrjs2000@naver.com>
2017-06-10 16:25:18 +0900

    XML 이런식으로 뿌리면 됩니다. ㄹ하이팅

--- FoodtruckFinderProject/src/fofa/controller/mobile/MobileMemberController.java
+++ FoodtruckFinderProject/src/fofa/controller/mobile/MobileMemberController.java
@@ -1,5 +1,6 @@
 package fofa.controller.mobile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import fofa.domain.Follow;
 import fofa.domain.Member;
+import fofa.domain.Members;
 import fofa.domain.Review;
 import fofa.domain.Reviews;
 import fofa.domain.Seller;
+import fofa.service.FollowService;
 import fofa.service.MemberService;
 import fofa.service.ReviewService;
 import fofa.service.SellerService;
@@ -23,7 +27,8 @@ public class MobileMemberController {
 	private MemberService memberService;
 	@Autowired
 	private ReviewService reviewService;
-
+	@Autowired
+	private FollowService followService;
 	
 	@RequestMapping(value="/login.do")
 	public @ResponseBody String memberLogin(String id, String password) {
@@ -51,11 +56,25 @@ public class MobileMemberController {
 		return reviews;
 	}
 
-	@RequestMapping(value = "review/followReview.do", produces="application/xml")
+	@RequestMapping(value = "/review/followReview.do", produces="application/xml")
 	public @ResponseBody Reviews searchReviewByFollow(String memberId){
 		List<Review> review = reviewService.findByFromId(memberId);
 		Reviews reviews = new Reviews();
 		reviews.setReviews(review);
 		return reviews;
+	}
+	
+	@RequestMapping(value = "/follow/list.do", produces="application/xml")
+	public @ResponseBody Members searchFollowers(String toId){
+		List<Follow> follow = followService.findFollow(toId);
+		List<Member> member = new ArrayList<>();
+		for(Follow f : follow){
+			member.add(memberService.findById(f.getToId()));
+		}
+		
+		Members members = new Members();
+		members.setMembers(member);
+		
+		return members;
 	}
 }
