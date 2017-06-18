a120d8e7a75d4340759e64a6acbb635e50a57cf2
Hyojin Kim <hg9829@gmail.com>
2017-06-19 01:00:06 +0900

    희희

--- FoodtruckFinderProject/src/fofa/controller/web/FoodtruckController.java
+++ FoodtruckFinderProject/src/fofa/controller/web/FoodtruckController.java
@@ -267,14 +267,12 @@ public class FoodtruckController {
 		model.addAttribute("stand", sort);
 		int currentIndex = 1;
 		boolean state = Boolean.parseBoolean(request.getParameter("openstate"));
-//		String state = request.getParameter("openstate");
 		
 		if(request.getParameter("currentIndex") != null){
 			currentIndex = Integer.parseInt(request.getParameter("currentIndex"));
 		}
 		if(state){
-//			System.out.println("state true");
-			foodtruck.setState(state);
+			foodtruck.setState(true);
 			model.addAttribute("openState", true);
 		}
 		
@@ -361,8 +359,8 @@ public class FoodtruckController {
 			t.setSpot((String)sqlMap.get(i).get("spot"));
 			t.setLocation((String)sqlMap.get(i).get("location"));
 			t.setFavoriteCount((int)sqlMap.get(i).get("favoriteCount"));
-			t.setFavoriteCount((int)sqlMap.get(i).get("favoriteCount"));
 			t.setReviewCount((int)sqlMap.get(i).get("reviewCount"));
+			t.setState((boolean)sqlMap.get(i).get("state"));
 			if(sqlMap.get(i).get("score")!=null){
 				t.setScore((double)sqlMap.get(i).get("score"));
 			}else {
